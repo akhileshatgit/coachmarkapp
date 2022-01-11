660d1716eb79da1b5223791a330fb64226a8b927
Akhilesh Sharma <akhileshsharma@Akhileshs-MacBook-Air.local>
2022-01-11 23:49:17 +0530

    coach mark added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useRef, useState } from "react";
+import { CoachMark, ICoachProps } from "react-coach-mark";
 
 function App() {
-  return (
+    
+    const ref1 = useRef(null);
+    const coach: ICoachProps = {
+            activate: true,
+            component: <div>Any Component You Want</div>,
+            reference: ref1,
+            tooltip: { position: 'bottom' }
+        };
+    <CoachMark {...coach} />
+  
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p ref={ref1}>Any ref based element</p>
         <p>
           This is an instructional overlay display POC.
         </p>
